ac58e340f40a56b25b53f251c3f8a1f0a401176b
Garrett Potts <gpotts@radiantblue.com>
2017-05-24 10:48:35 -0400

    Adding in attempts

--- plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroFileProcessingJob.groovy
+++ plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroFileProcessingJob.groovy
@@ -26,32 +26,52 @@ class AvroFileProcessingJob {
                ingestMetricsService.endIngest(fileRecord.processId)
                break
             case "post":
-               sleep( config.stagingDelay ) // ensure that the NFS has enough time to flush bits 
+               //sleep( config.stagingDelay ) // ensure that the NFS has enough time to flush bits 
                String url   = config.destination.post.addRasterEndPoint
                String field = config.destination.post.addRasterEndPointField
                HashMap params = config.destination.post.addRasterEndPointParams as HashMap
                params.filename = fileRecord.filename
                log.info "posting ${params} to URL: ${url}"
-               def result   = HttpUtils.postMessage(url, params)
-               log.info "Message posted"
-               // is a 200 range response
-               //
-               if((result?.status >= 200) && (result?.status <300))
+
+               Boolean stopAttempts = false
+               Integer nAttempts = config.nAttempts?:3
+               Integer attemptDelay = config.attemptDelay?:5000
+               Integer attempt = 1
+               while(!stopAttempts)
                {
-                  log.info "Posted Successfully: ${fileRecord.filename} to ${url}"
-                  avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully posted")
-               }
-               else if(result?.status == 415)
-               {
-                  log.error "Unable to index FILE: ${fileRecord.filename}.  Typically caused by the file is already present"
-                  avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File recognized but was not added")
-               }
-               else
-               {
-                  log.error result?.status
-                  log.error "${result?.message} Post failed to ${url} for  ${fileRecord.filename} with post field ${field}"
-                  avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, "Failed to post file to stager")
-                  ingestMetricsService.setStatus(fileRecord.processId, ProcessStatus.FAILED.toString(), "Failed to post file to stager")
+                  def result   = HttpUtils.postMessage(url, params)
+                  log.info "Message posted"
+                  // is a 200 range response
+                  //
+                  if((result?.status >= 200) && (result?.status <300))
+                  {
+                     log.info "Posted Successfully: ${fileRecord.filename} to ${url}"
+                     avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully posted")
+                     stopAttempts = true
+                  }
+                  else if(result?.status == 415)
+                  {
+                     log.error "Unable to index FILE: ${fileRecord.filename}.  Typically caused by the file is already present"
+                     avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File recognized but was not added")
+                     stopAttempts = true
+                  }
+                  else
+                  {
+                     if(nAttemps < attempt)
+                     {
+                        log.info "Attempt ${attempt} failed ... Trying again"
+                        ++attempt
+                     }
+                     else
+                     {
+                        stopAttempts = true
+                        log.error result?.status
+                        log.error "${result?.message} Post failed to ${url} for ${fileRecord.filename} with post field ${field}"
+                        avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, "Failed to post file to stager")
+                        ingestMetricsService.setStatus(fileRecord.processId, ProcessStatus.FAILED.toString(), "Failed to post file to stager")
+                     }
+                  }
+
                }
                break
          }
