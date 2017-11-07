399d4aef305ee18d9ff6171307b4ef69e7e223bb
Dylan-Cannisi <Dylan.Cannisi@digitalglobe.com>
2017-11-07 11:40:14 -0500

    Added post to avro-metadata

--- openshift.sh
+++
@@ -1,9 +0,0 @@
-# OMAR COMMON PROPERTIES PATH
-export OMAR_COMMON_PROPERTIES=/Users/dylancannisi/omar/omar-common/omar-common-properties.gradle
-
-# OPENSHIFT LOGIN INFO
-export OPENSHIFT_USERNAME=admin
-export OPENSHIFT_PASSWORD="P@ssw()rd123"
-
-
-$*
