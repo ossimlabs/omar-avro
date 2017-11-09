4425da5810d3aa58da1e6c4936e1fbc8dd7da19e
anies <arocnies@gmail.com>
2017-11-08 20:43:17 -0500

    Test fix for logging issue

--- plugins/omar-avro-plugin/src/main/groovy/omar/avro/HttpUtils.groovy
+++ plugins/omar-avro-plugin/src/main/groovy/omar/avro/HttpUtils.groovy
@@ -111,12 +111,12 @@ class HttpUtils
          http.handler.failure = { resp ->
             result.status = resp.status
             result.message = resp.statusLine
-            log.error "${result.message}"
+            log.error(result.message)
          }
          http.post(path: "/", body: body, requestContentType: TEXT) { resp ->
              result.message = resp.statusLine
              result.status = resp.statusLine.statusCode
-            log.info ${result.message}
+            log.info(result.message)
          }
       }
       catch (e)
@@ -124,7 +124,7 @@ class HttpUtils
          result.status = HttpStatus.NOT_FOUND
          result.message = e.toString()
          
-         log.error ${result.message}
+         log.error(result.message)
       }
       return result
    }
