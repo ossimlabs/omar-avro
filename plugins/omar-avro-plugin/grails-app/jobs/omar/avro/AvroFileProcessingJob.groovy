f31160c1c1e11aa517bf1278e732acd7c116b898
Garrett Potts <gpotts@radiantblue.com>
2017-12-21 17:00:11 -0500

    Adde try catch blocks for the entire execute just in case

--- plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroFileProcessingJob.groovy
+++ plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroFileProcessingJob.groovy
@@ -13,71 +13,77 @@ class AvroFileProcessingJob {
    def execute() {
       log.trace "Entered........."
 
-      def fileRecord
-      Boolean errorFlag = false
-      def config = OmarAvroUtils.avroConfig
-      def destinationType = config.destination.type.toLowerCase()
-      while(fileRecord = avroService.nextFile())
-      {
-         switch(destinationType)
+      try{
+         def fileRecord
+         Boolean errorFlag = false
+         def config = OmarAvroUtils.avroConfig
+         def destinationType = config.destination.type.toLowerCase()
+         while(fileRecord = avroService.nextFile())
          {
-            case "stdout":
-               log.info "FINISHED ${fileRecord.filename}"
-               avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully output")
-               ingestMetricsService.endIngest(fileRecord.processId)
-               break
-            case "post":
-               //sleep( config.stagingDelay ) // ensure that the NFS has enough time to flush bits
-               String url   = config.destination.post.addRasterEndPoint
-               String field = config.destination.post.addRasterEndPointField
-               HashMap params = config.destination.post.addRasterEndPointParams as HashMap
-               params.filename = fileRecord.filename
-               log.info "posting ${params} to URL: ${url}"
+            switch(destinationType)
+            {
+               case "stdout":
+                  log.info "FINISHED ${fileRecord.filename}"
+                  avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully output")
+                  ingestMetricsService.endIngest(fileRecord.processId)
+                  break
+               case "post":
+                  //sleep( config.stagingDelay ) // ensure that the NFS has enough time to flush bits
+                  String url   = config.destination.post.addRasterEndPoint
+                  String field = config.destination.post.addRasterEndPointField
+                  HashMap params = config.destination.post.addRasterEndPointParams as HashMap
+                  params.filename = fileRecord.filename
+                  log.info "posting ${params} to URL: ${url}"
 
-               Boolean stopAttempts = false
-               Integer nAttempts = config.nAttempts?:3
-               Integer attemptDelay = config.attemptDelay?:5000
-               Integer attempt = 0
+                  Boolean stopAttempts = false
+                  Integer nAttempts = config.nAttempts?:3
+                  Integer attemptDelay = config.attemptDelay?:5000
+                  Integer attempt = 0
 
-               while(!stopAttempts&&(attempt < nAttempts))
-               {
-                  def result   = HttpUtils.postMessage(url, params)
-                  // is a 200 range response
-                  //
-                  if((result?.status >= 200) && (result?.status <300))
+                  while(!stopAttempts&&(attempt < nAttempts))
                   {
-                     log.info "Posted Successfully: ${fileRecord.filename} to ${url}"
-                     avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully posted")
-                     stopAttempts = true
-                  }
-                  else if(result?.status == 415)
-                  {
-                     log.error "Unable to index FILE: ${fileRecord.filename}.  Typically caused by the file is already present"
-                     avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File recognized but was not added")
-                     stopAttempts = true
-                  }
-                  else
-                  {
-                     ++attempt
-                     if(attempt < nAttempts)
+                     def result   = HttpUtils.postMessage(url, params)
+                     // is a 200 range response
+                     //
+                     if((result?.status >= 200) && (result?.status <300))
                      {
-                        log.info "Attempt ${attempt} failed ... Trying again"
-                        sleep(attemptDelay)
+                        log.info "Posted Successfully: ${fileRecord.filename} to ${url}"
+                        avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File successfully posted")
+                        stopAttempts = true
+                     }
+                     else if(result?.status == 415)
+                     {
+                        log.error "Unable to index FILE: ${fileRecord.filename}.  Typically caused by the file is already present"
+                        avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FINISHED, "File recognized but was not added")
+                        stopAttempts = true
                      }
                      else
                      {
-                        log.error "${result?.status}"
-                        log.error "${result?.message} Post failed to ${url} for ${fileRecord.filename} with post field ${field}"
-                        avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, "Failed to post file to stager")
-                        ingestMetricsService.setStatus(fileRecord.processId, ProcessStatus.FAILED.toString(), "Failed to post file to stager")
+                        ++attempt
+                        if(attempt < nAttempts)
+                        {
+                           log.info "Attempt ${attempt} failed ... Trying again"
+                           sleep(attemptDelay)
+                        }
+                        else
+                        {
+                           log.error "${result?.status}"
+                           log.error "${result?.message} Post failed to ${url} for ${fileRecord.filename} with post field ${field}"
+                           avroService.updateFileStatus(fileRecord.processId, ProcessStatus.FAILED, "Failed to post file to stager")
+                           ingestMetricsService.setStatus(fileRecord.processId, ProcessStatus.FAILED.toString(), "Failed to post file to stager")
+                        }
                      }
-                  }
 
-               }
-               break
+                  }
+                  break
+            }
          }
       }
-
+      catch(e)
+      {
+         log.error e.toString()
+      }
+      
       log.trace "Leaving........."
    }
 }
--- plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroMessageIndexJob.groovy
+++ plugins/omar-avro-plugin/grails-app/jobs/omar/avro/AvroMessageIndexJob.groovy
@@ -22,138 +22,146 @@ class AvroMessageIndexJob {
       def ingestdate
       def avro_logs
       def messageRecordsToRetry = []
+
+      try{
+
 //      def config = OmarAvroUtils.avroConfig
-      while(messageRecord = avroService.nextMessage())
-      {
-        String messageId = messageRecord.messageId
-        ingestMetricsService.startCopy(messageId)
+        while(messageRecord = avroService.nextMessage())
+        {
+          String messageId = messageRecord.messageId
+          ingestMetricsService.startCopy(messageId)
 
-        ingestdate = new Date().format("yyyy-MM-dd hh:mm:ss.ms")
+          ingestdate = new Date().format("yyyy-MM-dd hh:mm:ss.ms")
 
-        starttime = System.currentTimeMillis()
+          starttime = System.currentTimeMillis()
 
-        try {
+          try {
 
-          def jsonObj
-          
-          try{
-            jsonObj = avroService.convertMessageToJsonWithSubField(messageRecord.message)
+            def jsonObj
+            
+            try{
+              jsonObj = avroService.convertMessageToJsonWithSubField(messageRecord.message)
 
-           // println jsonObj
-            // actual image information is in a subfield of the root JSON object
-          }
-          catch(e)
-          {
-            avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to parse message.  Not a valid JSON format")
-            log.error "Bad Json format.  Message will be ignored!"
-          
-          }
-          
-          String sourceURI = jsonObj?."${OmarAvroUtils.avroConfig.sourceUriField}"?:""
-          if(sourceURI)
-          {
-          
-            String prefixPath = "${OmarAvroUtils.avroConfig.download.directory}"
-            File fullPathLocation = avroService.getFullPathFromMessage(messageRecord.message)
-            File testPath = fullPathLocation.parentFile
-            HashMap tryToCreateDirectoryConfig = [
-                    numberOfAttempts:OmarAvroUtils.avroConfig.createDirectoryRetry.toInteger(),
-                    sleepInMillis: OmarAvroUtils.avroConfig.createDirectoryRetryWaitInMillis.toInteger()
-                    ]
-
-            if(AvroMessageUtils.tryToCreateDirectory(testPath, tryToCreateDirectoryConfig))
+             // println jsonObj
+              // actual image information is in a subfield of the root JSON object
+            }
+            catch(e)
+            {
+              avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to parse message.  Not a valid JSON format")
+              log.error "Bad Json format.  Message will be ignored!"
+            
+            }
+            
+            String sourceURI = jsonObj?."${OmarAvroUtils.avroConfig.sourceUriField}"?:""
+            if(sourceURI)
             {
             
-              try
+              String prefixPath = "${OmarAvroUtils.avroConfig.download.directory}"
+              File fullPathLocation = avroService.getFullPathFromMessage(messageRecord.message)
+              File testPath = fullPathLocation.parentFile
+              HashMap tryToCreateDirectoryConfig = [
+                      numberOfAttempts:OmarAvroUtils.avroConfig.createDirectoryRetry.toInteger(),
+                      sleepInMillis: OmarAvroUtils.avroConfig.createDirectoryRetryWaitInMillis.toInteger()
+                      ]
+
+              if(AvroMessageUtils.tryToCreateDirectory(testPath, tryToCreateDirectoryConfig))
               {
-            
-                if(!fullPathLocation.exists())
+              
+                try
                 {
               
-//                  log.info "DOWNLOADING: ${sourceURI} to ${fullPathLocation}"
-                  String commandString = OmarAvroUtils.avroConfig.download?.command
-                  //println "COMMAND STRING === ${commandString}"
-              
-                  if(!commandString)
+                  if(!fullPathLocation.exists())
                   {
-                    HttpUtils.downloadURI(fullPathLocation.toString(), sourceURI)
+                
+  //                  log.info "DOWNLOADING: ${sourceURI} to ${fullPathLocation}"
+                    String commandString = OmarAvroUtils.avroConfig.download?.command
+                    //println "COMMAND STRING === ${commandString}"
+                
+                    if(!commandString)
+                    {
+                      HttpUtils.downloadURI(fullPathLocation.toString(), sourceURI)
+                    }
+                    else
+                    {
+                      HttpUtils.downloadURIShell(commandString, fullPathLocation.toString(), sourceURI)
+                    }
+                
+   //                 log.info "DOWNLOADED: ${sourceURI} to ${fullPathLocation}"
+
+                    avroService.updatePayloadStatus(messageId, ProcessStatus.FINISHED, "DOWNLOADED: ${sourceURI} to ${fullPathLocation}")
+                
                   }
                   else
                   {
-                    HttpUtils.downloadURIShell(commandString, fullPathLocation.toString(), sourceURI)
+                
+                    log.info "${fullPathLocation} already exists and will not be re-downloaded"
+                    avroService.updatePayloadStatus(messageId, ProcessStatus.FINISHED, "Already exists and will not be downloaded")
+                
                   }
-              
- //                 log.info "DOWNLOADED: ${sourceURI} to ${fullPathLocation}"
+                
+                  ingestMetricsService.endCopy(messageId)
+                  avroService.addFile(new IndexFileCommand(filename:fullPathLocation))
 
-                  avroService.updatePayloadStatus(messageId, ProcessStatus.FINISHED, "DOWNLOADED: ${sourceURI} to ${fullPathLocation}")
-              
+                  /* -- Dylan Thomas
+                   * Create avro-metadata here
+                   * Need to find imageID from jsonObj. Also save jsonObj.
+                   * ... going to break everything.
+                   */
+                  def addMetadataURL = OmarAvroUtils.avroConfig?.metadata?.addMetadataEndPoint
+                  if(addMetadataURL)
+                  {
+                    HttpUtils.postToAvroMetadata(addMetadataURL, messageRecord.message)
+                  }
                 }
-                else
+                catch(e)
                 {
-              
-                  log.info "${fullPathLocation} already exists and will not be re-downloaded"
-                  avroService.updatePayloadStatus(messageId, ProcessStatus.FINISHED, "Already exists and will not be downloaded")
-              
-                }
-              
-                ingestMetricsService.endCopy(messageId)
-                avroService.addFile(new IndexFileCommand(filename:fullPathLocation))
-
-                /* -- Dylan Thomas
-                 * Create avro-metadata here
-                 * Need to find imageID from jsonObj. Also save jsonObj.
-                 * ... going to break everything.
-                 */
-                def addMetadataURL = OmarAvroUtils.avroConfig?.metadata?.addMetadataEndPoint
-                if(addMetadataURL)
-                {
-                  HttpUtils.postToAvroMetadata(addMetadataURL, messageRecord.message)
+                  log.error "Unable to Download: ${sourceURI} to ${fullPathLocation}\nWith error: ${e}"
+                  if(fullPathLocation?.exists())
+                  {
+                    fullPathLocation?.delete()
+                  }
+                  avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to Download: ${sourceURI} to ${fullPathLocation} With error: ${e}")
+                  messageRecord = null
+                  ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(), "Unable to Download: ${sourceURI} to ${fullPathLocation} With error: ${e}".toString())
                 }
               }
-              catch(e)
+              else
               {
-                log.error "Unable to Download: ${sourceURI} to ${fullPathLocation}\nWith error: ${e}"
-                if(fullPathLocation?.exists())
-                {
-                  fullPathLocation?.delete()
-                }
-                avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to Download: ${sourceURI} to ${fullPathLocation} With error: ${e}")
+                log.error "Unable to create directory '${testPath}'. "
+                avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to create directory '${testPath}'.")
+                ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"Unable to create directory '${testPath}'.".toString())
                 messageRecord = null
-                ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(), "Unable to Download: ${sourceURI} to ${fullPathLocation} With error: ${e}".toString())
               }
+
+              endtime = System.currentTimeMillis()
+              procTime = endtime - starttime
+
+              avro_logs = new JsonBuilder(ingestdate: ingestdate, procTime: procTime, inboxuri: fullPathLocation.toString())
+
+              log.info avro_logs.toString()
+
             }
             else
             {
-              log.error "Unable to create directory '${testPath}'. "
-              avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "Unable to create directory '${testPath}'.")
-              ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"Unable to create directory '${testPath}'.".toString())
+              log.error "JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found."
+              avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found.")
+              ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found.".toString())
               messageRecord = null
             }
-
-            endtime = System.currentTimeMillis()
-            procTime = endtime - starttime
-
-            avro_logs = new JsonBuilder(ingestdate: ingestdate, procTime: procTime, inboxuri: fullPathLocation.toString())
-
-            log.info avro_logs.toString()
-
           }
-          else
+          catch(e)
           {
-            log.error "JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found."
-            avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found.")
-            ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"JSON is not a proper AVRO message. Field '${OmarAvroUtils.avroConfig.sourceUriField}' not found.".toString())
-            messageRecord = null
+            log.error "${e}"
+            avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "${messageId} has error: ${e}")
+            ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"${messageId} has error: ${e}".toString())
           }
         }
-        catch(e)
-        {
-          log.error "${e}"
-          avroService.updatePayloadStatus(messageId, ProcessStatus.FAILED, "${messageId} has error: ${e}")
-          ingestMetricsService.setStatus(messageId, ProcessStatus.FAILED.toString(),"${messageId} has error: ${e}".toString())
-        }
-      }
 
-      log.trace "Leaving........."
+        log.trace "Leaving........."
+      }
+    }
+    catch(e)
+    {
+      log.error e.toString()      
     }
 }
